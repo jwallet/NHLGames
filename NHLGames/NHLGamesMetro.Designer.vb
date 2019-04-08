c9f45688cc57b9e3dd869f73ffaa8aab1447bd89
jwallet <joseouellet@gmail.com>
2019-04-08 02:02:52 -0400

    1.4.0
    - added highlights
    - re-added hosts support
    - updated go-mlbam-proxy
    - added hosts script go-mlbam-hosts
    - fixed black screen issue when using the proxy and mpv player
    - updated module spotify
    
    close issue #243
    close issue #153
    close issue #237
    close issue #228
    close issue #229
    close issue #233

--- NHLGames/NHLGamesMetro.Designer.vb
+++ NHLGames/NHLGamesMetro.Designer.vb
@@ -241,7 +241,7 @@ Partial Class NHLGamesMetro
         Me.tabMenu.Location = New System.Drawing.Point(-1, 60)
         Me.tabMenu.Margin = New System.Windows.Forms.Padding(0)
         Me.tabMenu.Name = "tabMenu"
-        Me.tabMenu.SelectedIndex = 0
+        Me.tabMenu.SelectedIndex = 1
         Me.tabMenu.Size = New System.Drawing.Size(992, 560)
         Me.tabMenu.SizeMode = System.Windows.Forms.TabSizeMode.Fixed
         Me.tabMenu.Style = MetroFramework.MetroColorStyle.Blue
@@ -969,7 +969,8 @@ Partial Class NHLGamesMetro
         Me.txtStreamerArgs.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
         Me.txtStreamerArgs.Size = New System.Drawing.Size(514, 57)
         Me.txtStreamerArgs.TabIndex = 1310
-        Me.txtStreamerArgs.Text = "--hls-segment-threads=4 --hls-segment-attempts=30 --hls-segment-timeout=60"
+        Me.txtStreamerArgs.Text = "--hls-segment-threads=2 --hls-segment-attempts=9 --hls-segment-timeout=10 --hls-t" &
+    "imeout=180"
         '
         'flpPlayerArgs
         '
