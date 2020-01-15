dddd039df636015d12c1594674f25d05d6e110ea
zouhair <zouhair@users.noreply.github.com>
2020-01-15 15:57:44 -0500

    Update GameWatchArguments.vb (#264)
    * Update GameWatchArguments.vb
    
    * Update NHLGames/Objects/GameWatchArguments.vb
    
    Co-Authored-By: jwallet <wizik20@gmail.com>
    
    Co-authored-by: jwallet <wizik20@gmail.com>

--- NHLGames/Objects/GameWatchArguments.vb
+++ NHLGames/Objects/GameWatchArguments.vb
@@ -89,9 +89,9 @@ Namespace Objects
             Dim title = $"{GameTitle} - {Stream.Network} - {Quality.ToString()}"
             Select Case PlayerType
                 Case PlayerTypeEnum.Mpv
-                    args &= $"--force-window=immediate --title """"{title}"""" --user-agent=User-Agent=""""{Common.UserAgent}"""""
+                    args &= $"--force-window=immediate --title=""""{title}"""" --user-agent=User-Agent=""""{Common.UserAgent}"""""
                 Case PlayerTypeEnum.Vlc
-                    args &= $"--meta-title """"{title}"""" "
+                     args &= $"--meta-title=""""{title}"""" "
                     If IsProxyNecessary() Then args &= $"{VlcHttpProxyArgs()} "
                 Case PlayerTypeEnum.Mpc
             End Select
